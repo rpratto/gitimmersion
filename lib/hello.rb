2d01a2641374c5b30a51c62d081c8449ef974820
reneelea <reneepratto@gmail.com>
2016-04-19 02:54:58 +0000

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
 # Default is "World"
 #Author: Renee` Pratto (rpratto@uncc.edu)
-name = ARGV.first || "World"
+#name = ARGV.first || "World"
+puts "What's your name"
+my_name = gets.
 
 puts "Hello, #{name}!"
